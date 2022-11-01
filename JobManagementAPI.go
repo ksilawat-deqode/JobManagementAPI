dd8cf93d50c719bb1fd75e9942cef9e3d2c2b25c
Kishan Silawat <kishan.silawat@skyflow.com>
2022-11-01 17:09:19 +0530

    Added client IP log

--- JobManagementAPI.go
+++ JobManagementAPI.go
@@ -155,6 +155,11 @@ func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProx
 
 	log.Printf("%v-> Initiated with id: %v", id, id)
 
+	clientIpAddress, IpPresent := request.Headers["CF-Connecting-IP"]
+	if IpPresent {
+		log.Printf("%v-> Client IP address: %v", id, clientIpAddress)
+	}
+
 	vaultId := request.PathParameters["vaultID"]
 
 	authResponse := SkyflowAuthorization(token, vaultId, id)
